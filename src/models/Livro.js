3e13d1b7522645a2145dd3d8cedc3f1aef2be801
Bruno Tenório <tenoriobn@gmail.com>
2023-12-13 10:46:11 -0300

    Validação do Mongoose

--- src/models/Livro.js
+++ src/models/Livro.js
@@ -8,10 +8,18 @@ const livroSchema = new mongoose.Schema({
   },
   editora: { 
     type: String,
-    required: [true, "A editora é obrigatória"]
+    required: [true, "A editora é obrigatória"],
+    enum: {
+      values: ["Casa do código", "Alura"],
+      message: "A editora {VALUE} não é um valor permitido"
+    }
   },
   preco: { type: Number },
-  paginas: { type: Number },
+  paginas: { 
+    type: Number,
+    min: [10, "O número de páginas deve estar entre 10 e 5000. Valor fornecido: {VALUE}"],
+    max: [5000, "O número de páginas deve estar entre 10 e 5000. Valor fornecido: {VALUE}"]
+  },
   autor: {
     type: mongoose.Schema.Types.ObjectId, 
     ref: "autores", 
