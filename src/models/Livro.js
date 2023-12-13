975632dd10e9a6249875d07b5598e804b682f976
Bruno Tenório <tenoriobn@gmail.com>
2023-12-13 11:07:04 -0300

    Validação personalizada

--- src/models/Livro.js
+++ src/models/Livro.js
@@ -17,8 +17,12 @@ const livroSchema = new mongoose.Schema({
   preco: { type: Number },
   paginas: { 
     type: Number,
-    min: [10, "O número de páginas deve estar entre 10 e 5000. Valor fornecido: {VALUE}"],
-    max: [5000, "O número de páginas deve estar entre 10 e 5000. Valor fornecido: {VALUE}"]
+    validate: {
+      validator: (valor) => {
+        return valor >= 10 && valor <= 5000;
+      },
+      message: "O número de páginas deve estar entre 10 e 5000. Valor fornecido: {VALUE}"
+    }
   },
   autor: {
     type: mongoose.Schema.Types.ObjectId, 
