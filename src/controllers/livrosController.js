a03a77ec26b71fa1c65f927ff89d9b38284f01be
Bruno Tenório <tenoriobn@gmail.com>
2023-12-19 12:51:45 -0300

    Paginando uma rota

--- src/controllers/livrosController.js
+++ src/controllers/livrosController.js
@@ -1,11 +1,27 @@
 import NaoEncontrado from "../erros/NaoEncontrado.js";
+import RequisicaoIncorreta from "../erros/RequisicaoIncorreta.js";
 import { autores, livros } from "../models/index.js";
-
 class LivroController {
   static async listarLivros (req, res, next) {
     try {
-      const listaLivros = await livros.find({}).populate("autor").exec();
-      res.status(200).json(listaLivros);
+      let { limite = 5, pagina = 1 } = req.query;
+
+      limite = parseInt(limite);
+      pagina = parseInt(pagina);
+
+      if (limite > 0 && pagina > 0) {
+        const listaLivros = await livros.find({})
+          .skip((pagina - 1) * limite)
+          .limit(limite)
+          .populate("autor")
+          .exec()
+        ;
+
+        res.status(200).json(listaLivros);
+      } else {
+        next(new RequisicaoIncorreta());
+      }
+
     } catch (erro) {
       next(erro);
     }
