457091875c5b4954e382dd23913bc36f077a5bf2
Bruno Tenório <tenoriobn@gmail.com>
2023-12-19 17:20:26 -0300

    Ordenando os resultados

--- src/controllers/livrosController.js
+++ src/controllers/livrosController.js
@@ -1,16 +1,21 @@
 import NaoEncontrado from "../erros/NaoEncontrado.js";
 import RequisicaoIncorreta from "../erros/RequisicaoIncorreta.js";
 import { autores, livros } from "../models/index.js";
+
 class LivroController {
   static async listarLivros (req, res, next) {
     try {
-      let { limite = 5, pagina = 1 } = req.query;
+      let { limite = 5, pagina = 1, ordenacao = "_id: -1" } = req.query;
+
+      let [campoOrdenacao, ordem] = ordenacao.split(":");
 
       limite = parseInt(limite);
       pagina = parseInt(pagina);
+      ordem = parseInt(ordem);
 
       if (limite > 0 && pagina > 0) {
         const listaLivros = await livros.find({})
+          .sort({ [campoOrdenacao]: ordem })
           .skip((pagina - 1) * limite)
           .limit(limite)
           .populate("autor")
